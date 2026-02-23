3b5117e470fd38b5a18cc2590231df3886287705
feyzasagman <sagmanfeyza@gmail.com>
2026-02-23 21:24:24 +0300

    chore: initial project setup with Vite React + TS

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <h1>Web Tasarimi ve Programlama</h1>
+      <h2>LAB-1</h2>
+      <p>Ad Soyad: [Feyza Sağman]</p>
+      <p>Ogrenci No: [230541123]</p>
+    </div>
+  )
+}
+
+export default App
